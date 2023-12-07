980f5f464c1329ff78fd9c74bbe9251447169de5
jun6651 <julia810400@gmail.com>
2023-12-07 11:39:09 +0800

    make it better

--- index.py
+++ index.py
@@ -233,8 +233,11 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "我是林映均開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
     return make_response(jsonify({"fulfillmentText": info}))
 
 
